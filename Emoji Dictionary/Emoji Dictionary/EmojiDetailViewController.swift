c7d7a28cd9479a21fbe5673f694b86665e7813ce
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-02 22:43:48 -0500

    Finished version 1.0, apphas simple functionality with default emojis with descriptions

--- Emoji Dictionary/Emoji Dictionary/EmojiDetailViewController.swift
+++ Emoji Dictionary/Emoji Dictionary/EmojiDetailViewController.swift
@@ -11,10 +11,30 @@ import UIKit
 class EmojiDetailViewController: UIViewController {
 
     @IBOutlet weak var emojiLabel: UILabel!
-    var emoji = ""      // holds the emoji that has been selected
+    var emoji = ""                              // holds the emoji that has been selected
+    @IBOutlet weak var emojiDefinition: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         emojiLabel.text = emoji
+        setLabelsForDefaultEmojis(emote: emoji)
+    }
+    
+    func setLabelsForDefaultEmojis(emote: String) {
+        //["😀","😊","🔥","👍🏿","🇺🇸"]
+        switch emote {
+        case "😀":
+            emojiDefinition.text = "Pretty happy guy"
+        case "😊":
+            emojiDefinition.text = "Aw, shucks kinda feeling"
+        case "🔥":
+            emojiDefinition.text = "Things are heatin' up!"
+        case "👍🏿":
+            emojiDefinition.text = "Good job, dude!"
+        case "🇺🇸":
+            emojiDefinition.text = "U.S.A."
+        default:
+            emojiDefinition.text = "Dunno what this is, you should define it!"
+        }
     }
 }
