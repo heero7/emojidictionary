e055f0fd6d0ecc848e67943ecc33e8d5bd053f19
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-07 22:58:49 -0600

    Working on Add emoji implementation

--- Emoji Dictionary/Emoji Dictionary/EditEmojiViewController.swift
+++ Emoji Dictionary/Emoji Dictionary/EditEmojiViewController.swift
@@ -23,6 +23,10 @@ class EditEmojiViewController: UIViewController {
     }
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    //MARK: Properties
+    
+    // Emoji is used to pass data in prepare(for:sender:)
+    var newEmoji:Emoji?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,8 +50,28 @@ class EditEmojiViewController: UIViewController {
         super.prepare(for: segue, sender: sender)
         
         // checking if save button works
-        //guard let button = sender as?
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed.. cancelling", log: OSLog.default,
+                   type: .debug)
+            return
+        }
+        
+        let emoji = emojiTextfield.text ?? ""
+        let descr = descriptionTextfield.text ?? ""
+        //let category = categoryTextfield.text ?? ""
+        let year = yearTextfield.text ?? ""
+        
+        newEmoji = Emoji(emoji: emoji, description: descr, category: 0, dob: year)
     }
     
-
+    //MARK: Actions
+    
+    @IBAction func unwindToEmojiDetailView(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as?
+            EditEmojiViewController, let newEmoji = sourceViewController.newEmoji {
+            
+            //add an emoji
+            //let newIndexPath = IndexPath(row: emoji)
+        }
+    }
 }
